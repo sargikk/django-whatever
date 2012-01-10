e562cd212de8417b2d8a0c6cb2baa1f8bca4ad59
Ilya Baryshev <baryshev@gmail.com>
2012-01-10 23:28:19 +0400

    Missing dependency (versiontools imports django_any, but django_any won't work without django installed) #19

--- django_any/__init__.py
+++ django_any/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-__version__ = (0, 2, 1, 'final', 0)
-from django_any.forms import any_form_field, any_form
-from django_any.models import any_field, any_model
+__version__ = (0, 2, 2, 'final', 0)
+try:
+    from django_any.forms import any_form_field, any_form
+    from django_any.models import any_field, any_model
+except ImportError:
+    pass
 
