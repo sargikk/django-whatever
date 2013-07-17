1eea250e17a4336cc8439a296adb6f19ce6bb833
Sergey Efimov <sargikk@mail.ru>
2013-07-17 13:26:08 +0400

    fixed FileField path creation

--- django_any/models.py
+++ django_any/models.py
@@ -251,8 +251,8 @@ def any_file_field(field, **kwargs):
         upload_to = os.path.dirname(generated_filepath)
     else:
         upload_to = field.upload_to
-    if not os.path.exists(upload_to):
-        os.makedirs(upload_to)
+    if not os.path.exists(field.storage.base_location):
+        os.makedirs(field.storage.base_location)
     result = get_some_file(upload_to)
 
     if result is None and not field.null:
